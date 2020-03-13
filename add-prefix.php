1e15bba0c7e6b5de5030ded5a414fe43db7ced30
Khalid Almallahi <abukotsh@gmail.com>
2020-03-13 10:51:58 +0200

    Create add-prefix.php

---
+++ add-prefix.php
@@ -0,0 +1,19 @@
+/**
+ * @snippet       Add Prefix to Category @ WooCommerce Breadcrumb
+ * @author        Khalid Almallahi
+ * @testedwith    WooCommerce 3.9
+ * @donate $9     https://businessbloomer.com/bloomer-armada/
+ */
+ 
+add_filter( 'woocommerce_get_breadcrumb', 'bbloomer_single_product_edit_cat_breadcrumbs', 9999, 2 );
+ 
+function bbloomer_single_product_edit_cat_breadcrumbs( $crumbs, $breadcrumb ) {
+    
+   if ( is_product() ) {
+      $index = count( $crumbs ) - 2; // cat is always second last item
+      $value = $crumbs[$index];
+      $crumbs[$index][0] = 'Category: ' . $crumbs[$index][0];
+   }
+    
+   return $crumbs;
+}
